10509c1906256722588bdcad9568ac8054053686
Junior <juniorboe@hotmail.com>
2024-05-22 21:55:45 -0300

    c5 - Finalizando o dia de estudos da aula 3

--- Estrutura_de_dados_I/Rascunhos_e_testes/Aula3/lista.c
+++ Estrutura_de_dados_I/Rascunhos_e_testes/Aula3/lista.c
@@ -2,70 +2,99 @@
 #include <stdlib.h>
 #include <time.h>
 
+//Criando estrutura de nó
 typedef struct no {
-    int dado;
-    struct no *proximo;
+    int data;
+    struct no *nxt;
 } no;
 
-//Definindo variáveis
-typedef struct no *ptr_no;
-ptr_no lista;
-int op;
+//Criando variáveis
+typedef no *pt;
+pt list; //Criando a variável que apontará para o primeiro nó
+int op; //Variável de controle do laço while da main()
 
-//prototipação
-void menu_mostrar();
-void menu_selecionar(int op);
-void lista_inserir(ptr_no lista);
-void lista_mostrar(ptr_no lista);
+//Prototipação
+void show_menu();
+void show_list(pt list);
+void add_data(pt list);
+void rm_data(pt data);
+void select_menu(int op);
 
 //Função principal
 int main() {
+    //Inicializando o randomizador
+    srand(time(NULL));
+    //Criando o primeiro nó e apontando list para ele
+    list = (pt)malloc(sizeof(no)); //Alocando espaço para o nó e apontando list para ele
+    list->data = 0;
+    list->nxt = NULL;
+
+    //Criando laço while para rodar o programa
     op = 1;
-
-    lista = (ptr_no)malloc(sizeof(no));
-    lista->dado = 0;
-    lista->proximo = NULL;
-
     while (op != 0) {
-        system("cls");
-        menu_mostrar();
+        system("cls || clear");
+        show_menu();
         scanf("%d", &op);
-        menu_selecionar(op);
+        select_menu(op); 
     }
-    system("Pause");
     return 0;
 }
 
-void menu_mostrar() {
-    lista_mostrar(lista);
-    printf("\n\nEscolha uma opcao:\n");
+//Tratamento de funções
+void show_menu() {
+    show_list(list);
+    printf("\n\nEscolha uma opcao: \n");
     printf("1- Adicionar elemento.\n");
+    printf("2- Remover elemento.\n");
     printf("0- Sair.\n");
 }
 
-void lista_mostrar(ptr_no lista) {
-    system("cls");
-    while (lista->proximo != NULL) {
-        printf("%d, ", lista->dado);
-        lista = lista->proximo;
+void show_list(pt list) {
+    while (list->nxt != NULL) {
+        printf("%d, ", list->data);
+        list = list->nxt;
     }
-    printf("%d", lista->dado);
+    printf("%d", list->data);
 }
 
-void menu_selecionar(int op) {
+void select_menu(int op) {
     switch (op) {
         case 1:
-            lista_inserir(lista);
-        break;
+            add_data(list);
+            break;
+        case 2:
+            rm_data(list);
+            break;
     }
 }
 
-void lista_inserir(ptr_no lista) {
-    while (lista->proximo != NULL) {
-        lista = lista->proximo;
+void add_data(pt list) {
+    while (list->nxt != NULL) {
+        list = list->nxt;
     }
-    lista->proximo = (ptr_no)malloc(sizeof(no));
-    lista = lista->proximo;
-    lista->dado = 1;
-    lista->proximo = NULL;
+    int new_data;
+    printf("Digite um numero.");
+    scanf("%d", &new_data);
+    list->nxt = (pt)malloc(sizeof(no)); //Cria um novo nó para adicionar à lista
+    list = list->nxt;
+    list->data = new_data;
+    list->nxt = NULL;
+}
+
+void rm_data(pt list) {
+    int data_remove;
+    pt atual;
+    atual = (pt)malloc(sizeof(no));
+    printf("Qual numero voce quer remover da lista?\n");
+    scanf("%d", &data_remove);
+    while (list->nxt != NULL) {
+        if (list->data == data_remove) {
+            atual->nxt = list->nxt;
+        }
+        atual = list;
+        list = list->nxt;
+    }
+    if (list->data == data_remove) {
+        atual->nxt = NULL;
+    }
 }
